2e0e7060b4097b1e4898117e527338d7778928db
Roy Sand <roy@sanddata.no>
2024-12-02 18:59:53 +0100

    Added classes for reading json from AMS Leser

---
+++ DataLayer/src/Domain/Models/AmsLeser/AMSReaderData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace DataLayer.Domain.Models.AmsLeser;
+
+public class AMSReaderData
+{
+    public DateTime TimeStamp { get; } = DateTime.Now; 
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("up")]
+    public int Up { get; set; }
+
+    [JsonProperty("t")]
+    public int T { get; set; }
+
+    [JsonProperty("vcc")]
+    public double Vcc { get; set; }
+
+    [JsonProperty("rssi")]
+    public int Rssi { get; set; }
+
+    [JsonProperty("temp")]
+    public double Temp { get; set; }
+
+    [JsonProperty("data")]
+    public AMSReaderDataDetail Data { get; set; }    
+}
