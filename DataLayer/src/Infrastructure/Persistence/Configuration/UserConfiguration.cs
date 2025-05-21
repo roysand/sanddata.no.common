fe596d2f4e4139ccc494a7285a348ff9b29ce590
Roy Sand <roy@sanddata.no>
2025-05-21 22:36:59 +0200

    Corrected Location setup

--- DataLayer/src/Infrastructure/Persistence/Configuration/UserConfiguration.cs
+++ DataLayer/src/Infrastructure/Persistence/Configuration/UserConfiguration.cs
@@ -15,20 +15,5 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(e => e.UserId).ValueGeneratedNever();
         builder.Property(e => e.Email).HasMaxLength(100);
         builder.Property(e => e.UserName).HasMaxLength(100);
-
-        builder.HasMany(d => d.Locations).WithMany(p => p.Users)
-            .UsingEntity<Dictionary<string, object>>(
-                "UserLocation",
-                r => r.HasOne<Location>().WithMany()
-                    .HasForeignKey("LocationId")
-                    .OnDelete(DeleteBehavior.ClientSetNull),
-                l => l.HasOne<User>().WithMany()
-                    .HasForeignKey("UserId")
-                    .OnDelete(DeleteBehavior.ClientSetNull),
-                j =>
-                {
-                    j.HasKey("UserId", "LocationId");
-                    j.ToTable("UserLocation");
-                });
     }
 }
