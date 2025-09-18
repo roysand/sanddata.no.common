5bbb22e053f8e08810b2ba175fe820bcd708e260
Roy Sand <roy@sanddata.no>
2025-09-18 20:57:38 +0200

    Added some more code ...

--- DataLayer/src/Domain/Entities/AppUserLocation.cs
+++ DataLayer/src/Domain/Entities/AppUserLocation.cs
@@ -5,6 +5,6 @@ public class AppUserLocation
     public Guid AppUserId { get; set; }
     public AppUser AppUser { get; set; } = null!;
 
-    public int LocationId { get; set; }
+    public Guid LocationId { get; set; }
     public Location Location { get; set; } = null!;
 }
