4709cbefa36143badf4679c383c7a32b6be66a47
Roy Sand <roy@sanddata.no>
2025-09-18 21:29:15 +0200

    Added role for AppUser

---
+++ DataLayer/src/Infrastructure/Persistence/Configuration/AppUserRoleConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataLayer.Infrastructure.Persistence.Configuration;
+
+public class AppUserRoleConfiguration : IEntityTypeConfiguration<AppUserRole>
+{
+    public void Configure(EntityTypeBuilder<AppUserRole> builder)
+    {
+        builder.HasKey(e => new { e.AppUserId, e.RoleId }).HasName("appuser_role_pk");
+        builder.ToTable("appuser_role", "dbo");
+        
+        builder.HasOne(e => e.AppUser)
+            .WithMany(a => a.AppUserRoles)
+            .HasForeignKey(e => e.AppUserId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .HasConstraintName("FK_appuser_role");
+        
+        builder.HasOne(e => e.Role)
+            .WithMany(r => r.AppUserRoles)
+            .HasForeignKey(e => e.RoleId)
+            .HasConstraintName("FK_role_appuser")
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
